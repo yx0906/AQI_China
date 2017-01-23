b510f03af052bffeac519258c4c66982ff4a474b
yx0906 <midwestdc914@gmail.com>
2017-01-23 21:30:15 +0800

    Rename folder to app/

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
-import aqi_china
-#from aqi_china import data
-from aqi_china import automate
+import app
+from app import automate
 automate.collect_all()
